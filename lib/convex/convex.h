7e2d1d823764d86eac7eacf883e4eac543cf18d6
OT Chen <ot32em@gmail.com>
2014-07-28 02:44:47 +0800

    not tested yet convex implementation

--- lib/convex/convex.h
+++ lib/convex/convex.h
@@ -1,51 +1,51 @@
 #include <vector>
+#include <stdexcept>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include "../common.h"
 
 std::vector<Line> 
 get_convex(const std::vector<Point>& in_points)
 {
-    std::sort(in_points.begin(), in_points.end(), 
+    if(in_points.size() < 3)
+        throw std::invalid_argument("Convex needs at least 3 points.");
+
+    std::vector<Point> sorted_points = in_points;
+    std::sort(sorted_points.begin(), sorted_points.end(), 
         [](const Point& lhs, const Point& rhs){ 
             return lhs.x == rhs.x? lhs.y < rhs.y: lhs.x < rhs.x;
     });
+    std::list<Point> points(sorted_points.begin(), sorted_points.end());
 
-    // decide the first line, p1 to p2 as the line1
-    //
-    Point& p1 = in_points.front();
-    Point fake_p = {first_p.x, std::numeric_limits<double>::min()};
-    Line line0 = {fake_p, p1};
-
-    double min_cos = std::numeric_limits<double>::min():
-    std::size_t min_cos_p_i = 1;
-    for(std::size_t i=1; i < in_points.size(); i++)
+    Point curr_p = points.front();
+    points.pop_front();
+    Point prev_p = {curr_p.x, curr_p.y-1};
+    
+    for(Line ref_l = {prev_p, curr_p}; 
+        points.empty(); 
+        ref_l = {prev_p, curr_p})
     {
-        Line line = {p1, in_points[i]};
-        double cos = inner_dot(line0, line);
-        if(cos < min_cos)
+        auto selected_p_iter = points.begin();
+        double min_cos = std::numeric_limits<double>::max();
+        for(auto p_iter = points.begin(); p_iter != points.end(); p_iter++)
         {
-            min_cos = cos;
-            min_cos_p_i = i;
-        }
-    }
-    Line line1 = {p1, in_points[i];}
-
-
-
-    // compare each Line(p2, p) which p in p3, p4, ... pn 
-    // to Line(p1, p2). calculate the angle vec of Line1 and vec of Line2
-    for(std::size_t i= in_points.size(); i >= 0; i-0)
-    {
-        double min_cos = std::numeric_limtis<double>::min();
-        std::size_t min_cos_p_i= 3;
-        for(std::size_t j=3; j <= i; j++)
-        {
-            Line line = {p1, in_points[i]};
-            double cos = inner_dot(line0, line);
+            Line finding_l {curr_p, *p_iter};
+            double cos = ref_l.vec().x * finding_l.vec().x + 
+                         ref_l.vec().y * finding_l.vec().y;
             if(cos < min_cos)
             {
                 min_cos = cos;
-                min_cos_p_i = i;
+                selected_p_iter = p_iter;
             }
         }
-        Line line_found = {p1, in_points[j];}
+        prev_p = curr_p;
+        curr_p = *selected_p_iter;
+        points.erase(selected_p_iter);
     }
+
+
+}
+
+    // decide the first line, p1 to p2 as the line1
 
