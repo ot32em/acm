37568aa364fcd0d8e8d8aae20679900e7f5b04c5
OT Chen <ot32em@gmail.com>
2014-07-28 02:43:04 +0800

    correct the operator< of Point. Add vector method for Line

--- lib/common.h
+++ lib/common.h
@@ -4,12 +4,15 @@ struct Point
     double y;
 
     bool operator<(const Point& lhs) const { 
-        return x==lhs.x? y < lhs.y; x < lhs.x;
+        return (x == lhs.x)? (y < lhs.y): (x < lhs.x);
     }
 };
+
+typedef Point Vector;
 
 struct Line
 {
     Point src;
     Point dst;
+    Vector vec() const { return {dst.x - src.x, dst.y - src.y};}
 };
